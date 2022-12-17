cc9151a27e0b846629bf11b065391962a4c29988
Arkhatan-TheGods <pauloevertron@outlook.com>
2022-12-17 20:59:20 -0300

    subindo def de emprestimo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-Extração de dados na internet
+Extração de dados na internet sobre insetos hexapoda
+
 versão do python usada: 3.11.1
+
+Descreva os passos para virtualização do ambiente
+
+Incluir o texto abaixo para instalação das dependências
+
+Para a instalação das dependências, executar o seguinte comando:
+
+pip freeze > requirements.txt
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,14 @@
-def test_extrair_dados():
-    
-    assert 1 > 0
-    
-    print('h')
+salario = float(input('digite o salário: '))
+valor_emp = float(input('valor do empréstimo: '))
+parcelas = int(input('quantidade de parcelas'))
+
+def test_emprestimo(salario, valor_emp, parcelas):
+    dezporcento = salario * 0.10
+    totmes = valor_emp/parcelas
+    if totmes < dezporcento:
+        print('empréstimo aprovado.')  
+    else:
+        print('empréstimo negado.')
+        
+        
+test_emprestimo(salario, valor_emp, parcelas)
