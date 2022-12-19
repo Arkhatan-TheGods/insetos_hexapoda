c37d5974c6390fca74359aab4a6749b4b6e06cc4
Arkhatan-TheGods <pauloevertron@outlook.com>
2022-12-19 17:18:02 -0300

    verificar arquivo test.py

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,16 @@
 
-def emprestimo(salario:float, valor_emp:float, parcelas:int) -> bool:
+def emprestimo(salario: float, valor_emp: float, parcelas: int) -> bool:
 
     dezporcento = salario * 0.10
     total_mes = valor_emp/parcelas
 
     status_emprestimo = False
 
-    if total_mes < dezporcento: 
+    if total_mes < dezporcento:
         status_emprestimo = True
 
     return status_emprestimo
+
 
 def test_pass_emprestimo():
 
@@ -19,6 +20,7 @@ def test_pass_emprestimo():
 
     assert emprestimo(salario, valor_emp, parcelas) == True
 
+
 def test_failed_emprestimo():
 
     salario = 5000
@@ -26,4 +28,3 @@ def test_failed_emprestimo():
     parcelas = 24
 
     assert emprestimo(salario, valor_emp, parcelas) == False
-
