e41564ec67d67ee4e5759a9e418e2bd94fe2f92b
rodrigmars <rodrigmars@gmail.com>
2022-12-18 21:08:18 -0300

    Otimizando testes para funcionalidade de empréstimo

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,29 @@
 
-def test_emprestimo():
+def emprestimo(salario:float, valor_emp:float, parcelas:int) -> bool:
+
+    dezporcento = salario * 0.10
+    total_mes = valor_emp/parcelas
+
+    status_emprestimo = False
+
+    if total_mes < dezporcento: 
+        status_emprestimo = True
+
+    return status_emprestimo
+
+def test_pass_emprestimo():
+
+    salario = 5000
+    valor_emp = 4000
+    parcelas = 24
+
+    assert emprestimo(salario, valor_emp, parcelas) == True
+
+def test_failed_emprestimo():
+
     salario = 5000
     valor_emp = 20000
     parcelas = 24
-    dezporcento = salario * 0.10
-    totmes = valor_emp/parcelas
-    if totmes < dezporcento:
-        print('empréstimo aprovado.')
-    else:
-        print('empréstimo negado.')
+
+    assert emprestimo(salario, valor_emp, parcelas) == False
+
