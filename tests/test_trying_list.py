8e94cf210332ee1ce44a4b99069cb717adf69a17
Arkhatan-TheGods <pauloevertron@outlook.com>
2022-12-24 14:22:34 -0300

    novas diferenças.

--- tests/test_trying_list.py
+++ tests/test_trying_list.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta, time, date
+from datetime import date, time, datetime, timedelta
+import pytest
 
 dados = [['Start Time', 'Lunch Start', 'Lunch End', 'End Time', 'user ID'],
          ['08:25:00',    '12:15:00',  '12:50:00', '19:20:00',     '452'],
@@ -13,62 +14,59 @@ dados = [['Start Time', 'Lunch Start', 'Lunch End', 'End Time', 'user ID'],
          ['09:15:00',    '12:02:00',          '', '18:25:00',     '211'],
          ['08:25:00',    '13:25:00',  '12:15:00', '18:28:00',     '187']]
 
-dados[0].append('Worked Hours')
-dados[0].append('Time in Lunch')
-
-start_time = []
-lunch_start = []
-lunch_end = []
-end_time = []
-ID = []
-worked_hours = []
-time_in_lunch = []
-time_in_company = []
-
-
-for lista in dados:
-    # print(lista[0])
-    start_time.append(lista[0])
-    lunch_start.append(lista[1])
-    lunch_end.append(lista[2])
-    end_time.append(lista[3])
-    ID.append(lista[4])
-
-
-c = 0
-while c < 11:
-    c += 1
-    if start_time[c] in '':
-        print(f'O ID: {ID[c]} não bateu ponto de entrada.')
-    if lunch_start[c] in '':
-        print(f'O ID: {ID[c]}, não informou ida para almoço')
-    elif lunch_end[c] in '':
-        print(f'O ID: {ID[c]} não informou volta do almoço')
-    elif lunch_start[c] in '' and lunch_end[c] in '':
-        print(f'O ID: {ID[c]} não foi almoçar.')
-    if end_time[c] in '':
-        print(f'O ID: {ID[c]} não bateu ponto de saída.')
-    if start_time[c] not in '' and end_time[c] not in '':
-        entrada = datetime.strptime(start_time[c], '%H:%M:%S')
-        saida = datetime.strptime(end_time[c], '%H:%M:%S')
+for pos, valor in enumerate(dados[1:]):
+    if valor[0] in '':
+        print(f'O ID: {valor[4]} não registrou entrada no serviço.')
+    if valor[1] in '':
+        print(f'O ID: {valor[4]} não registrou entrada no almoço.')
+    elif valor[2] in '':
+        print(f'O ID: {valor[4]} não registrou saída do almoço.')
+    elif valor[1] in '' and valor[2] in '':
+        print(f'O ID: {valor[4]} não comeu.')
+    if valor[3] in '':
+        print(f'O ID: {valor[4]} não informa saída do trabalho.')
+    if valor[0] not in '' and valor[3] not in '':
+        entrada = datetime.strptime(valor[0], '%H:%M:%S')
+        saida = datetime.strptime(valor[3], '%H:%M:%S')
         calculo = saida - entrada
         if calculo < timedelta(days=0):
             calculo += timedelta(days=1)
-        print(f'O ID: {ID[c]} trabalhou ao total: {calculo}.')
+        if valor[1] in '' or valor[2] in '':
+            calculo -= timedelta(hours=1)
+        print(f'O ID: {valor[4]} trabalhou ao total: {calculo}.')
 
-        # print(f'ID: {ID[c]}')
 
-        # c = 0
-        # for t in zip(start_time[c], end_time[c]):
-        #    c += 1
-        #    print(f'funcionário de ID: {identity[c]} entrou às {start_time[c]}, foi almoçar {lunch_start[c]}, saiu do almoço {lunch_end[c]} e saiu às {end_time[c]}.')
+@pytest.fixture(scope='function')
+def setup():
+    lista = ['08:25:00', '12:15:00', '12:50:00', '19:20:00', '452']
+    yield lista
 
-        # for indice, listas in enumerate(dados[1:]):
-        #    print(indice, listas)
-        #    for c in listas:
-        #        #print(c)
-        #         print(f''''o funcionário de ID:{c[4]}')
-        #         começou o trabalho às {c[0]},
-        #         almoçou às {c[1]},
-        #         saiu do almoço às {c[2]},
-        #         e foi embora às {c[3]}.''')
+def test_pass_lista_carregada(setup) -> None:
+    lista = setup
+    assert lista != []
+    assert lista
+    assert len(lista) > 0
+    assert type(lista) == list
+    assert isinstance(lista, list)
+
+def test_pass_str_in_list(setup) -> str:
+   lista = setup
+   for i, e in enumerate(lista):
+       assert type(e) == str, f'na posição {i} é esperado uma informação em formato string.'
+
+def test_entrada_trabalho(setup):
+    entrada_trabalho = setup[0]
+    assert entrada_trabalho != None
+
+def test_entrada_almoco(setup):
+    entrada_almoco = setup[1]
+    assert entrada_almoco != None
+
+def test_saida_almoco(setup):
+    saida_almoco = setup[2]
+    assert saida_almoco != None
+
+def test_saida_trabalho(setup):
+    saida_trabalho = setup[3]
+    assert saida_trabalho != None
+    
