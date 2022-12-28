caef5335b4d1b565964fe150bf570bb37a6130a4
rodrigmars <rodrigmars@gmail.com>
2022-12-28 10:22:28 -0300

    Testes criados para products core

---
+++ tests/core/tests_product_core.py
@@ -0,0 +1,32 @@
+import pytest
+import insetos_hexapoda.core.product_core as product_core
+from typing import Type, Mapping, List, Dict, Tuple, Any
+
+@pytest.fixture(scope='module')
+def setup():
+
+    mock_products = [[1, 'Tênis Ultimate TKYX', 150.50, True],
+                     [2, 'Tênis BRBRBR-AAA', 360.50, True],
+                     [3, 'Tênis BRBRBR-PNB', 145.90, False],
+                     [4, 'Tênis SSSS-T', 220.50, False],
+                     [5, 'Tênis XV-123', 175.90, False]]
+
+    yield mock_products
+
+
+def test_pass_product_not_available(setup) -> None:
+
+    product_id: int = 5
+
+    produto:Mapping[int, list[Any]] = product_core.available_search_product(setup, product_id)
+
+    assert not produto
+
+
+def test_pass_product_available(setup) -> None:
+
+    product_id:int = 1
+
+    produto:Mapping[int, list[Any]] = product_core.available_search_product(setup, product_id)
+
+    assert produto
