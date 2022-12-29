0df9761f690177c22ad0b03026407d29c07b3698
Arkhatan-TheGods <pauloevertron@outlook.com>
2022-12-29 11:53:01 -0300

    test_trying_list.py

--- tests/test_trying_list.py
+++ tests/test_trying_list.py
@@ -1,113 +1,69 @@
-from datetime import date, time, datetime, timedelta
-import pytest
+import csv
+import os
+import pickle
+from datetime import datetime, time, timedelta
+
+caminho_arquivo = os.chdir("data\\")
+
+# abrindo arquivo, lendo os dados, passando para lista.
+with open('Time_Tracking.csv', encoding='utf-8') as arquivo:
+    lendo_dados = csv.reader(arquivo)
+    dados = list(lendo_dados)
+    # print(dados)
 
 
-dados = [['Start Time', 'Lunch Start', 'Lunch End', 'End Time', 'user ID'],
-         ['08:25:00',    '12:15:00',  '12:50:00', '19:20:00',     '452'],
-         ['15:40:00',    '12:00:00',  '12:45:00', '17:55:00',     '485'],
-         ['07:45:00',    '11:40:00',  '12:33:00', '17:55:00',     '155'],
-         ['18:40:00',    '12:25:00',  '12:48:00', '18:20:00',     '854'],
-         ['09:10:00',    '12:13:00',  '13:00:00', '10:25:00',     ' 54'],
-         ['08:45:00',    '12:25:00',  '13:20:00', '18:10:00',     '201'],
-         ['',            '12:10:00',  '13:00:00', '18:05:00',     '120'],
-         ['08:15:00',            '',          '', '18:02:00',     '325'],
-         ['10:00:00',    '12:10:00',  '13:05:00',         '',     '424'],
-         ['09:15:00',    '12:02:00',          '', '18:25:00',     '211'],
-         ['08:25:00',    '13:25:00',  '12:15:00', '18:28:00',     '187']]
-
-lista = []
-for pos, valor in enumerate(dados[1:]):
-    if not valor[0]:
-        print(f'O ID: {valor[4]} não registrou entrada no serviço.')
-    if not valor[1]:
-        print(f'O ID: {valor[4]} não registrou entrada no almoço.')
-    elif not valor[2]:
-        print(f'O ID: {valor[4]} não registrou saída do almoço.')
-    if not valor[3]:
-        print(f'O ID: {valor[4]} não informa saída do trabalho.')
-    if valor[0] and valor[3]:
-        entrada = datetime.strptime(valor[0], '%H:%M:%S')
-        saida = datetime.strptime(valor[3], '%H:%M:%S')
-        calculo = saida - entrada
-        if calculo < timedelta(days=0):
-            calculo += timedelta(days=1)
-        if not valor[1] or not valor[2]:
-            calculo -= timedelta(hours=1)
-        if not valor[1] and not valor[2]:
-            calculo += timedelta(hours=1)
-        print(f'O ID: {valor[4]} trabalhou ao total: {calculo}.')
-        dicio = {'ID': valor[4], 'Entrada serviço:': valor[0], 'Entrada Almoço:': valor[1],
-                 'Saída Almoço:': valor[2], 'Saída serviço:':valor[3] }
-        lista.append(dicio)
-#print(lista)
+"""dados = [['date', 'Start Time', 'Lunch Start', 'Lunch End', 'End Time', 'user ID'],
+         ['24/08/2022', '08:25', '12:15', '12:50', '19:20', '452'],
+         ['15/07/2022', '07:35', '12:00', '', '17:55', '485'],
+         ['03/08/2022', '07:45', '11:40', '12:33', '17:55', '155'],
+         ['15/06/2022', '', '', '12:48', '18:20', '854'],
+         ['20/07/2022', '09:10', '12:13', '13:00', '17:35', '54'],
+         ['07/06/2022', '08:45', '12:25', '13:20', '', '201'],
+         ['08/08/2022', '', '12:10', '13:00', '18:05', '120'],
+         ['17/08/2022', '08:15', '', '', '18:02', '325'],
+         ['07/09/2022', '10:00', '12:10', '13:05', '', '424'],
+         ['18/05/2022', '09:15', '12:02', '', '18:25', '211'],
+         ['05/09/2022', '08:25', '12:05', '14:50', '18:28', '187'],
+         ['11/11/2022', '06:24', '11:35', '12:22', '17:14', '875'],
+         ['30/09/2022', '07:17', '11:25', '12:30', '18:03', '785'],
+         ['16/05/2022', '11:22', '14:18', '15:22', '20:50', '124'],
+         ['05/10/2022', '09:02', '', '13:05', '18:15', '35']]"""
 
 
-@pytest.fixture(scope='function')
-def setup():
-    lista = ['08:25:00', '12:15:00', '12:50:00', '19:20:00', '452']
-    yield lista
+nova_lista = []
+horas_trabalhadas = []
+horas_lunch = []
 
+for c in dados[1:]:
+    dicio = {'data:': c[0],
+             'entrada trabalho:': c[1],
+             'entrada almoço:': c[2],
+             'saida almoço:': c[3],
+             'saida trabalho:': c[4],
+             'user id:': c[5], }
+    nova_lista.append(dicio)
+    for k, v in dicio.items():
+        if v == "":
+            dicio[k] = "sem dado"
 
-def test_pass_lista_carregada(setup) -> None:
-    lista = setup
-    assert lista != []
-    assert lista
-    assert len(lista) > 0
-    assert type(lista) == list
-    assert isinstance(lista, list)
+for v in nova_lista:
+    dt_formato = '%d/%m/%Y%H:%M'
+    if v['entrada trabalho:'] not in 'sem dado' and v['saida trabalho:'] not in 'sem dado':
+        entrada = v['data:'] + v['entrada trabalho:']
+        entrada_dt = datetime.strptime(entrada, dt_formato)
+        saida = v['data:'] + v['saida trabalho:']
+        saida_dt = datetime.strptime(saida, dt_formato)
+        conta = saida_dt - entrada_dt
+        horas_trabalhadas.append(conta)
+        v.update({'tempo trabalhado:': str(conta)})
 
+    if v['entrada almoço:'] not in 'sem dado' and v['saida almoço:'] not in 'sem dado':
+        entrada = v['data:'] + v['entrada almoço:']
+        entrada_dt = datetime.strptime(entrada, dt_formato)
+        saida = v['data:'] + v['saida almoço:']
+        saida_dt = datetime.strptime(saida, dt_formato)
+        conta = saida_dt - entrada_dt
+        v.update({'tempo no almoço:': str(conta)})
 
-def test_pass_str_in_list(setup) -> str:
-    lista = setup
-    for i, e in enumerate(lista):
-        assert type(
-            e) == str, f'na posição {i} é esperado uma informação em formato string.'
-
-
-def test_entrada_trabalho(setup):
-    entrada_trabalho = setup[0]
-    assert entrada_trabalho != None
-
-
-def test_entrada_almoco(setup):
-    entrada_almoco = setup[1]
-    assert entrada_almoco != None
-
-
-def test_saida_almoco(setup):
-    saida_almoco = setup[2]
-    assert saida_almoco != None
-
-
-def test_saida_trabalho(setup):
-    saida_trabalho = setup[3]
-    assert saida_trabalho != None
-
-
-def test_convertendo_str_datetime(setup):
-    entrada = setup[0]
-    entrada_dt = datetime.strptime(entrada, '%H:%M:%S')
-    lunch = setup[1]
-    lunch_dt = datetime.strptime(lunch, '%H:%M:%S')
-    lunch_end = setup[2]
-    lunch_end_dt = datetime.strptime(lunch_end, '%H:%M:%S')
-    saida = setup[3]
-    saida_dt = datetime.strptime(saida, '%H:%M:%S')
-    assert isinstance(entrada_dt, datetime)
-    assert isinstance(lunch_dt, datetime)
-    assert isinstance(lunch_end_dt, datetime)
-    assert isinstance(saida_dt, datetime)
-
-
-def test_validando_calculo_datetime(setup):
-    entrada = setup[0]
-    entrada_dt = datetime.strptime(entrada, '%H:%M:%S')
-    saida = setup[3]
-    saida_dt = datetime.strptime(saida, '%H:%M:%S')
-    calculo = saida_dt - entrada_dt
-
-    if calculo < timedelta(days=0):
-        calculo += timedelta(days=1)
-
-    calculo_str = str(calculo)
-    assert calculo_str == '10:55:00'
+for v in nova_lista:
+    print(v['user id:'])
