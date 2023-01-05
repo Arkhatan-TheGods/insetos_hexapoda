6f2dd7b918828e06e9e41c88c2a8cb7d0f81a471
Arkhatan-TheGods <pauloevertron@outlook.com>
2023-01-05 18:51:04 -0300

    updates

--- tests/web_scraping/on_begin.py
+++ tests/web_scraping/on_begin.py
@@ -1,4 +1,36 @@
 import requests
+import pytest
+import json
+from bs4 import BeautifulSoup
 
-link = requests.get('https://pt.wikipedia.org/wiki/Hexapoda')
-print(link)
+hexapoda_link = 'https://pt.wikipedia.org/wiki/Hexapoda'
+
+
+def header():
+    page = requests.get('https://pt.wikipedia.org/wiki/Hexapoda')
+    content = BeautifulSoup(page.content, 'html.parser')
+    # show_content = content.prettify()
+    # print(show_content)
+    return content
+
+
+header()
+
+
+def find_all_element(element: str):
+    finding = str(element)
+    content = header()
+    list(content.children)
+    print(content.find_all(finding))
+
+
+# @pytest.fixture
+# def setup_header():
+#     page = requests.get('https://pt.wikipedia.org/wiki/Hexapoda')
+#     content = BeautifulSoup(page.content, 'html.parser')
+#     return content
+
+
+# def test_show_content():
+#     content = setup_header()
+#     print(content)
