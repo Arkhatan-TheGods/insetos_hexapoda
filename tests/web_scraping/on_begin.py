c04ceb9d650530c633baf533750705b488ef311b
Arkhatan-TheGods <pauloevertron@outlook.com>
2023-01-06 16:20:37 -0300

    adicionado funções de buscas

--- tests/web_scraping/on_begin.py
+++ tests/web_scraping/on_begin.py
@@ -1,36 +1,61 @@
 import requests
-import pytest
-import json
 from bs4 import BeautifulSoup
+import json
 
-hexapoda_link = 'https://pt.wikipedia.org/wiki/Hexapoda'
+"""foram importados os módulos, agora criar processos divididos de baixar a página, transformar em beautifulsoup, depois criar uma ferramenta de buscar todos os elementos html, depois outra função de busca."""
 
+def main_link():
+  link = 'https://pt.wikipedia.org/wiki/Hexapoda'
+  return link
 
-def header():
-    page = requests.get('https://pt.wikipedia.org/wiki/Hexapoda')
-    content = BeautifulSoup(page.content, 'html.parser')
-    # show_content = content.prettify()
-    # print(show_content)
-    return content
+def requesting():
+  link = main_link()
+  page = requests.get(link)
+  return page
 
+def show_content():
+  page = requesting()
+  content = page.content
+  return content
 
-header()
+def content_to_bs():
+  content = show_content()
+  bs = BeautifulSoup(content, 'html.parser')
+  return bs
 
+def find_all_tags(tag:str): # faz uma busca por todas as tags
+  bs = content_to_bs()
+  target = tag
+  find = bs.find_all(target)
+  found = find
+  return found
 
-def find_all_element(element: str):
-    finding = str(element)
-    content = header()
-    list(content.children)
-    print(content.find_all(finding))
+def tags_to_text(tag:str): # extrai texto de tags buscada no parâmetro
+  bs = content_to_bs()
+  target = tag
+  find = bs.find_all(target)
+  for pos, each_tag in enumerate(find):
+    print(pos ,str(each_tag.get_text()))
 
+"""próximo objetivo, achar a posição da tag conhecida como tbody para depois pegar as classes e subdivisões dos hexapoda, como exemplo: Collembola, Protura, Diplura... """
 
-# @pytest.fixture
-# def setup_header():
-#     page = requests.get('https://pt.wikipedia.org/wiki/Hexapoda')
-#     content = BeautifulSoup(page.content, 'html.parser')
-#     return content
+def show_links(): # mostra os links de todos as tags <a> com href
+  bs = content_to_bs()
+  find= bs.find_all('a',href=True)
+  for pos, links in enumerate(find):
+    print(pos, links.get('href'))
+#show_links()
 
+def scientific_classification():
+    for pos, element in enumerate(find_all_tags('tbody')):
+      if pos == 2:
+        print(str(element.get_text()))
 
-# def test_show_content():
-#     content = setup_header()
-#     print(content)
+def cladograma():
+  for pos, element in enumerate(find_all_tags('tbody')):
+      if pos == 3:
+        print(str(element.get_text()))
+
+#tags_to_text('tbody')
+
+#find_all_tags('tbody')
